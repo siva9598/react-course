d9f35980bf4ed70505fffe56c443889f09e71bd3
siva9598 <sivasubramanian6996@gmail.com>
2021-09-20 18:39:14 +0530

    added the files for the first module

---
+++ one-helloworld/src/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import Button from "./button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Button title="play store" />;
+      <Button title="app store" />;
+    </div>
+  );
+};
+
+export default App;
