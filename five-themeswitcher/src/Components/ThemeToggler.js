c7c883f149a534a0ca3195f4e274ed72b32ac944
siva9598 <sivasubramanian6996@gmail.com>
2021-09-20 22:10:17 +0530

    added fifth module files

---
+++ five-themeswitcher/src/Components/ThemeToggler.js
@@ -0,0 +1,17 @@
+import React, {useContext} from "react";
+import ThemeContext from "../Context/ThemeContext";
+
+const ThemeToggler = () => {
+    const [themeMode,setThemeMode] = useContext(ThemeContext);
+    return(
+        <button
+        onClick = {()=> {
+            setThemeMode(themeMode === "light" ? "dark" : "light")
+        }}
+        >
+        <span>{themeMode === "light" ? "Turn Off" : "Light On"}</span>
+        </button>
+    ); 
+};
+
+export default ThemeToggler;
