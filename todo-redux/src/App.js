6f8e2bba08ccc59f638c7887b63ace97e2eb75d8
siva9598 <sivasubramanian6996@gmail.com>
2021-09-20 22:16:22 +0530

    added todo redux module files

---
+++ todo-redux/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import {Container} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Todo from "./components/Todo"
+import TodoForm from "./components/TodoForm"
+import {Provider} from "react-redux"
+import store from "./store"
+function App() {
+  return (
+   <Provider store = {store}>
+     <Container fluid >
+       <Todo/>
+       <TodoForm/>
+     </Container>
+   </Provider>
+  );
+}
+
+export default App;
